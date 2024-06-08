772f4922f9c9ddc87f72c2b22387b1b10ca71d95
carlos18bp <carlos18bp@gmail.com>
2024-06-08 15:27:48 +0000

    Initial commit with frontend setup and configuration

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <RouterView/>
+</template>
+
+<script setup>
+  import { onMounted } from 'vue'
+  import { RouterView } from "vue-router";
+  import { useProductStore } from '@/store/product';
+
+  // Initialize the product store
+  const prductStore = useProductStore()
+
+  /**
+   * Fetch the list of products from the store when the component is mounted.
+   */
+  onMounted(() => {
+    prductStore.fetchProducts()
+  })
+</script>
---
+++ frontend/src/views/product/Catalog.vue
@@ -0,0 +1,10 @@
+<template>
+    <Header></Header>
+    <h1>CATALOG VIEW</h1>
+    <Footer></Footer>    
+</template>
+
+<script setup>
+    import Header from "@/components/layouts/Header.vue";
+    import Footer from "@/components/layouts/Footer.vue";
+</script>
