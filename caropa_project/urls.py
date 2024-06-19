e4bd6d9a065587915cfb8b1c22f4c3f1dfa19e6f
carlos18bp <carlos18bp@gmail.com>
2024-06-19 23:08:20 +0000

    feat: Add custom structure for the admin side.

--- caropa_project/urls.py
+++ caropa_project/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from django.conf import settings
 from django.contrib import admin
+from caropa_app.admin import admin_site
 from django.conf.urls.static import static
 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin-gallery/', admin.site.urls),
+    path('admin/', admin_site.urls),
     path('api/', include('caropa_app.urls')),
 ]
 
